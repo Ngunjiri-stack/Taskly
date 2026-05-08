bcb507ac0a0e0fa55cf327154e34f63314dda66b
Ngunjiri-stack <ngunjiri.18427@students.kyu.ac.ke>
2026-05-09 00:13:32 +0300

    Add CRUD endpoints for task management

--- app/main.py
+++ app/main.py
@@ -1,1 +1,64 @@
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
 
+from .database import SessionLocal, engine, Base
+from . import models, schemas, crud
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI(title="Taskly")
+
+
+# Database Dependency
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.get("/")
+def home():
+    return {"message": "Taskly Running"}
+
+
+@app.post("/tasks", response_model=schemas.Task)
+def create_task(task: schemas.TaskCreate, db: Session = Depends(get_db)):
+    return crud.create_task(db, task)
+
+
+@app.get("/tasks", response_model=list[schemas.Task])
+def read_tasks(db: Session = Depends(get_db)):
+    return crud.get_tasks(db)
+
+
+@app.get("/tasks/{task_id}", response_model=schemas.Task)
+def read_task(task_id: int, db: Session = Depends(get_db)):
+    task = crud.get_task(db, task_id)
+
+    if not task:
+        raise HTTPException(status_code=404, detail="Task not found")
+
+    return task
+
+
+@app.put("/tasks/{task_id}")
+def update_task(task_id: int, completed: bool, db: Session = Depends(get_db)):
+    task = crud.update_task(db, task_id, completed)
+
+    if not task:
+        raise HTTPException(status_code=404, detail="Task not found")
+
+    return {"message": "Task updated"}
+
+
+@app.delete("/tasks/{task_id}")
+def delete_task(task_id: int, db: Session = Depends(get_db)):
+    task = crud.delete_task(db, task_id)
+
+    if not task:
+        raise HTTPException(status_code=404, detail="Task not found")
+
+    return {"message": "Task deleted"}
